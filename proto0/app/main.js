423f0585b0d6f49fd4ecf1d8e75b60be277d5365
mizchi <miz404@gmail.com>
2018-06-22 12:16:34 +0900

    Add ui

--- proto0/app/main.js
+++ proto0/app/main.js
@@ -28,10 +28,16 @@ class App extends React.Component {
         name: "John"
       },
       filepath: "./components/Greeting.js",
-      component: null
+      component: null,
+      files: []
     };
   }
   async componentDidMount() {
+    const res = await fetch("/manifest.json");
+    const manifest = await res.json();
+    console.log(manifest);
+    this.setState({ files: manifest.files });
+
     // check ts
     const { default: bar } = await import("./misc/bar.ts");
     console.log("bar", bar(3));
@@ -44,10 +50,34 @@ class App extends React.Component {
     const { default: component } = await import(this.state.filepath);
     this.setState({ component });
   }
+
+  async componentDidUpdate(_, prev) {
+    // check react
+    if (prev.filepath !== this.state.filepath) {
+      const { default: component } = await import(this.state.filepath);
+      this.setState({ component });
+    }
+  }
+
   render() {
     if (this.state.component) {
       return (
         <div>
+          {this.state.files.length > 0 && (
+            <>
+              <ul>
+                {this.state.files.map(file => {
+                  return (
+                    <li key={file}>
+                      <button onClick={() => this.setState({ filepath: file })}>
+                        {file}
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          )}
           <PropsEditor
             initialValue={this.state.props}
             onChange={props => this.setState({ props })}
--- proto0/sw/index.js
+++ proto0/sw/index.js
@@ -1,20 +1,8 @@
-import * as ts from "typescript";
-import pkg from "../package.json";
+import path from "path";
+import { transformWithBabel } from "./transformWithBabel";
+import { transformWithTypeScript } from "./transformWithTypescript";
 
 console.log("sw installed");
-
-const compilerOptions = {
-  module: ts.ModuleKind.ESNext,
-  sourcemap: false,
-  jsx: "react"
-};
-
-import { transform } from "@babel/core/lib/transform";
-import flow from "@babel/preset-flow";
-import react from "@babel/preset-react";
-import pluginSyntaxDynamicImport from "@babel/plugin-syntax-dynamic-import";
-
-import rewriteModulePath from "./rewriteModulePath";
 
 self.addEventListener("install", function(event) {
   event.waitUntil(self.skipWaiting());
@@ -24,78 +12,48 @@ self.addEventListener("activate", function(event) {
   event.waitUntil(self.clients.claim());
 });
 
-const fetchBabeledJS = async url => {
-  const res = await fetch(url);
-  const source = await res.text();
-  const output = transform(source, {
-    presets: [flow, react],
-    plugins: [
-      pluginSyntaxDynamicImport,
-      [
-        rewriteModulePath,
-        {
-          filename: url
-        }
-      ]
-    ]
-  }).code;
-  console.log("compiled", output);
-  return new Response(output, {
-    mode: "no-cors",
-    headers: { "Content-Type": "text/javascript" }
-  });
-};
+setupForChrome();
 
-const fetchTypeScriptJS = async url => {
-  const res = await fetch(url);
-  const source = await res.text();
+function setupForChrome() {
+  const compilers = {
+    ".js": transformWithBabel,
+    ".ts": transformWithTypeScript,
+    ".tsx": transformWithTypeScript
+  };
 
-  const tsCompiled = ts.transpileModule(source, { compilerOptions });
-
-  // rewrite module path
-  const output = transform(tsCompiled.outputText, {
-    plugins: [
-      [
-        rewriteModulePath,
-        {
-          filename: url
-        }
-      ]
-    ]
-  }).code;
-
-  console.log("compiled", output);
-  return new Response(output, {
-    mode: "no-cors",
-    headers: { "Content-Type": "text/javascript" }
-  });
-};
-
-self.addEventListener("fetch", event => {
-  // cache jspm
-  if (event.request.url.indexOf("jspm.io") > -1) {
-    event.respondWith(
-      caches.match(event.request).then(res => {
-        return (
-          res ||
-          caches.open("1").then(cache => {
-            return fetch(event.request).then(response => {
-              console.log("put", event.request.url);
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          })
+  self.addEventListener("fetch", event => {
+    // cache jspm
+    if (event.request.url.indexOf("jspm.io") > -1) {
+      event.respondWith(
+        caches.match(event.request).then(res => {
+          return (
+            res ||
+            caches.open("1").then(cache => {
+              return fetch(event.request).then(response => {
+                console.log("put", event.request.url);
+                cache.put(event.request, response.clone());
+                return response;
+              });
+            })
+          );
+        })
+      );
+    } else {
+      const extname = path.extname(event.request.url);
+      const compiler = compilers[extname];
+      if (compiler) {
+        event.respondWith(
+          fetch(event.request.url)
+            .then(res => res.text())
+            .then(source => {
+              const output = compiler(source);
+              return new Response(output, {
+                mode: "no-cors",
+                headers: { "Content-Type": "text/javascript" }
+              });
+            })
         );
-      })
-    );
-  } else if (event.request.url.indexOf(".js") > -1) {
-    console.log("transform by babel", event.request.url);
-    event.respondWith(fetchBabeledJS(event.request.url));
-  } else if (
-    event.request.url.indexOf(".ts") > -1 ||
-    event.request.url.indexOf(".tsx") > -1
-  ) {
-    console.log("transform by typescript", event.request.url);
-    event.respondWith(fetchTypeScriptJS(event.request.url));
-  }
-});
+      }
+    }
+  });
+}
